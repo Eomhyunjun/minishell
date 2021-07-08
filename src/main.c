c0ab22d4add436e610fa72d63052ab5d55b95121
Hyun Jun Eom <zzp1318@gmail.com>
2021-07-08 19:23:17 +0900

    missing push this is main about signal

--- src/main.c
+++ src/main.c
@@ -6,53 +6,12 @@
 /*   By: heom <heom@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/21 14:19:09 by heom              #+#    #+#             */
-/*   Updated: 2021/07/08 19:17:05 by heom             ###   ########.fr       */
+/*   Updated: 2021/07/08 19:23:06 by heom             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <fcntl.h>
 #include "egginshell.h"
-
-int custom_rl_getc_fuction(FILE *stream)
-{
-	int result;
-	unsigned char c;
-	(void)(stream);
-
-	while (1)
-	{
-		result = read(rl_instream->_file, &c, sizeof(unsigned char));
-		if (c == 4 && *rl_line_buffer == '\0')
-		{
-			rl_on_new_line();
-			write(1, "exit\n", 5);
-			exit(0);
-		}
-
-		if (result == sizeof(unsigned char))
-			return (c);
-		if (result == 0)
-		{
-			printf("result is 0\n");
-			return (EOF);
-		}
-	}
-}
-
-void sigint_handler(int signo)
-{
-	(void)signo;
-	write(1, "\n", 1);
-	rl_replace_line("", 0);
-	rl_on_new_line();
-	if (!all()->cmd_info)
-		rl_redisplay();
-}
-
-void do_nothing(int signo)
-{
-	(void)signo;
-}
 
 int		main(int argc, char **argv, char **envp)
 {
@@ -61,10 +20,6 @@ int		main(int argc, char **argv, char **envp)
 	char	*buf;
 
 	init(envp);
-	signal(SIGINT, sigint_handler);
-	signal(SIGQUIT, do_nothing);
-	rl_getc_function = custom_rl_getc_fuction;
-	rl_catch_signals = 0;
 	while (1)
 	{
 		safe_free_cmd();
